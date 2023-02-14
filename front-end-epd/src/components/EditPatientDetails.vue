7ec8217bc5d0d8aca77250aa830f85031931a46b
Mattijs Pronk <456410@student.fontys.nl>
2023-02-14 21:16:55 +0100

    EditPatientDetails fixes

--- front-end-epd/src/components/EditPatientDetails.vue
+++ front-end-epd/src/components/EditPatientDetails.vue
@@ -4,25 +4,40 @@
     <div class="box">
             <h1>Edit patient</h1>
             <span v-on:click="closeEditPatientDetails()" class="icon-btn-close"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg></span>
-            <hr/><br/>   
+            <hr/>  
             <p class="p-trunc">
             <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg></span>
             Patient details: 
-            </p>
+            </p><br/>
 
-            <img src="../components/icons/hasbullah.png" alt="image"><br/><br/>
-            <p1 class="description">
-                <p2 class="details">Patients name:</p2><br/>
-                <input type="text" id="title" class="field" placeholder="Name">
-            </p1><br/>
-            <p1 class="description">
-                <p2 class="details">DOB:</p2><br/>
-                <input type="text" id="title" class="field" placeholder="Date of Birth">
-            </p1><br/>
-            <p1 class="description">
-                <p2 class="details">CSN:</p2><br/>
-                <input type="text" id="title" class="field" placeholder="Citizen Service Number">
-            </p1><br/>
+            <div class="inputBox">
+                <input class="inputBox-field" type="text" required="required">
+                <span>Image URL</span>
+            </div>
+            <div class="inputBox">
+                <input class="inputBox-field" type="text" required="required">
+                <span>FullName</span>
+            </div>
+            <div class="inputBox">
+                <input class="inputBox-field" type="text" required="required">
+                <span>DateOfBirth</span>
+            </div>
+            <div class="inputBox">
+                <input class="inputBox-field" type="text" required="required">
+                <span>CitizenServiceNumber</span>
+            </div>
+            <div class="inputBox">
+                <input class="inputBox-field" type="text" required="required">
+                <span>Adress</span>
+            </div>
+            <div class="inputBox">
+                <input class="inputBox-field" type="text" required="required">
+                <span>PhoneNumber</span>
+            </div>
+            <div class="inputBox">
+                <input class="inputBox-field" type="text" required="required">
+                <span>Email</span>
+            </div>
             <p1 class="description">
                 <p2 class="details">Insured:</p2><br/>
                 <label class="switch">
@@ -30,23 +45,11 @@
                       <span class="slider round"></span>
                 </label>
             </p1><br/>
-            <p1 class="description">
-                <p2 class="details">Adress:</p2><br/>
-                <input type="text" id="title" class="field" placeholder="Current adress">
-            </p1><br/>
-            <p1 class="description">
-                <p2 class="details">Phone:</p2><br/>
-                <input type="text" id="title" class="field" placeholder="Phone number">
-            </p1><br/>
-            <p1 class="description">
-                <p2 class="details">Email:</p2><br/>
-                <input type="text" id="title" class="field" placeholder="Email adress">
-            </p1><br/>
             <a class="btn-save">
                 <span class="icon-btn-save"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V173.3c0-17-6.7-33.3-18.7-45.3L352 50.7C340 38.7 323.7 32 306.7 32H64zm0 96c0-17.7 14.3-32 32-32H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V128zM224 288a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg></span>
                 <span> Save</span>
             </a> 
-            <a class="btn-cancel">
+            <a class="btn-cancel" v-on:click="closeEditPatientDetails()">
                 <span class="icon-btn-cancel"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg></span>
                 <span> Cancel</span>
             </a> 
@@ -61,6 +64,7 @@ export default{
     props: [
         'getEditPatientdetails'
     ],
+    emits: ["changeEditPatientDetailsComponent"],
     methods: {
         closeEditPatientDetails(){
             this.$emit('changeEditPatientDetailsComponent', true)
--- front-end-epd/src/components/PatientDetails.vue
+++ front-end-epd/src/components/PatientDetails.vue
@@ -7,10 +7,10 @@ import EditPatientDetails from '../components/EditPatientDetails.vue';
 <section v-if="getPatientdetails">
     <div class="container" id="background">
 
-    <div class="btn-return" v-on:click="switchActiveComponent()">
+    <button v-bind:disabled="enableBtn" class="btn-return" v-on:click="switchActiveComponent()">
         <span class="icon-btn-return"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></span>
         <span> Return</span>
-    </div>
+    </button>
     <br/>
         <div class="box2">
             <h1>Patient Dossier</h1>
@@ -45,10 +45,10 @@ import EditPatientDetails from '../components/EditPatientDetails.vue';
             <p1 class="description">
                 <p2 class="details">Hospital:</p2> Pillbox medical
             </p1><br/>
-            <a class="btn-edit" v-on:click="opendEditPatientDetails()">
+            <button v-bind:disabled="enableBtn" class="btn-edit" v-on:click="opendEditPatientDetails()">
                 <span class="icon-btn-edit"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg></span>
                 <span> Edit</span>
-            </a> 
+            </button> 
             
 
             <br/><br/><br/><hr/>
@@ -60,10 +60,10 @@ import EditPatientDetails from '../components/EditPatientDetails.vue';
             <p1 class="description">
                 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintocksick
             </p1><br/>
-            <a class="btn-edit">
+            <button v-bind:disabled="enableBtn" class="btn-edit">
                 <span class="icon-btn-edit"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg></span>
                 <span> Edit</span>
-            </a> 
+            </button> 
     
             <br/><br/><br/><hr/>
     
@@ -74,18 +74,18 @@ import EditPatientDetails from '../components/EditPatientDetails.vue';
             <p1 class="description">
                 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
             </p1><br/>
-            <a class="btn-edit">
+            <button v-bind:disabled="enableBtn" class="btn-edit">
                 <span class="icon-btn-edit"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg></span>
                 <span> Edit</span>
-            </a>        
+            </button>        
         </div>
     
         <div class="box3">
             <h2>Patients history</h2>   
-            <a class="btn">
+            <button v-bind:disabled="enableBtn" class="btn">
                 <span class="icon-btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M240 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H176V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H240V80z"/></svg></span>
                 <span> Add</span>
-            </a>  
+            </button>  
             <br/>
             <div class="box4">
                 <h2>Gebroken been</h2>
@@ -145,20 +145,24 @@ export default{
     data(){
         return{
             editpatientdetails: false,
+            enableBtn: false
         }
     },
     props: [
         'getPatientdetails'
     ],
+    emits: ["changePatientDetailsComponent"],
     methods:{
         switchActiveComponent(){
             this.$emit('changePatientDetailsComponent', true)
         },
         closeEditPatientDetails(){
+            this.enableBtn = false;
             this.editpatientdetails = false;
             document.getElementById('background').style.filter = 'none'
         },
         opendEditPatientDetails(){
+            this.enableBtn = true;
             this.editpatientdetails = true;
             document.getElementById('background').style.filter = 'blur(7px)'
         }
